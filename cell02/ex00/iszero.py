4c48e6403b6afc484ab763562014be158f407438
pachuakh <66080197@kmitl.ac.th>
2025-09-16 19:26:33 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,11 @@
+def main():
+    try:
+        number = float(input("Enter a number: "))
+        if number == 0:
+            print("This number is equal to zero.")
+        else:
+            print("This number is different from zero.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+if __name__== "__main__":
+    main()
